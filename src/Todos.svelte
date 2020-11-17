d016bbdca15ac66d8142f6853ed39d2f3655bfb5
Sunny Dasgupta <SunnyDasgupta11@gmail.com>
2020-11-17 21:49:20 +0000

    Finished tutorial

--- src/Todos.svelte
+++ src/Todos.svelte
@@ -23,8 +23,97 @@ import TodoItem from './Todoitem.svelte';
             title: 'My third todo',
             completed: false
         }
-    ]
+    ];
+
+    function addTodo(event) {
+        if (event.key === 'Enter') {
+            todos = [...todos, {
+                id: nextId, 
+                completed: false, 
+                title: newTodoTitle
+            }];
+            nextId = nextId + 1;
+            newTodoTitle = '';
+        }
+    }
+
+    $: todosRemaining = filteredTodos.filter(todo => !todo.completed).length
+    $: filteredTodos = currentFilter === 'all' ? todos :currentFilter === 'completed'
+        ? todos.filter(todo =>todo.completed)
+        : todos.filter(todo => !todo.completed)
+
+    function checkAllTodos(event) {
+        todos.forEach(todo => todo.completed = event.target.checked);
+        todos = todos;
+    }
+
+    function updateFilter(newFilter) {
+        currentFilter = newFilter;
+    }
+
+    function clearCompleted() {
+        todos = todos.filter(todo => !todo.completed)
+    }
+
+    function handleDeleteTodo(event) {
+        todos = todos.filter(todo => todo.id !== event.detail.id);
+    }
+
+    function handleToggleComplete(event) {
+        const todoIndex = todos.findIndex(todo => todo.id === event.detail.id);
+        const updatedTodo = { ...todos[todoIndex], completed: !todos[todoIndex].completed}
+
+        todos = [
+            ...todos.slice(0, todoIndex), 
+            updatedTodo, 
+            ...todos.slice(todoIndex+1)
+        ];
+    }
 </script>
+
+<style>
+    .container {
+        max-width: 800px;
+        margin: 10px auto;
+    }
+    .logo {
+        display: block;
+        margin: 20px auto;
+        width: 50%;
+    }
+    .todo-input {
+        width: 100%;
+        padding: 10px, 20px;
+        font-size: 18px;
+        margin-bottom: 20px;
+    }
+    .inner-container {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        font-size: 16px;
+        border-top: 1px solid lightgrey;
+        padding-top: 15px;
+        margin-bottom: 13px;
+    }
+    .inner-container-input {
+        margin-right: 12px;
+    }
+    button {
+        font-size: 14px;
+        background-color: white;
+        appearance: none;
+    }
+    button:hover {
+        background: lightseagreen;
+    }
+    button:focus {
+        outline: none;
+    }
+    .active {
+        background: lightseagreen;
+    }
+</style>
 
 <div class="container">
     <img src={'/img/CTSWLogo.png'} alt="CodingTheSmartWay Logo">
@@ -54,5 +143,3 @@ import TodoItem from './Todoitem.svelte';
         </div>
     </div>
 </div>
-<!-- 
-Stopped at 36:47 -->
